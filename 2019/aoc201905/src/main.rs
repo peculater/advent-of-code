c70bda3ee475254682c7ab592d26291b5b6a991f
William Lieurance <william.lieurance@namikoda.com>
2019-12-06 02:07:35 -0600

    Day 5 part 2

--- 2019/aoc201905/src/main.rs
+++ 2019/aoc201905/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 
-const INPUT: i32 = 1;
+const INPUT: i32 = 5;
 
 fn main() {
     let file = BufReader::new(File::open("input").unwrap());
@@ -25,27 +25,31 @@ fn run<'a>(program: &'a mut Vec<i32>) -> i32 {
     if program[i] == 99 { 
         break; 
     }
-    execute(program, i);
-    i += match program[i] % 100 {
+    let this_instruction = program[i] % 100;
+    let this_i = i.clone();
+    execute(program, &mut i);
+    i += match this_instruction {
         1 => 4,
         2 => 4,
         3 => 2,
         4 => 2,
-        _ => panic!("Unknown instruction length for {} at {}", program[i], i)
+        5 => 3,
+        6 => 3,
+        7 => 4,
+        8 => 4,
+        _ => panic!("Unknown instruction length for {} at {}", this_instruction, this_i)
     }
   }
 
   return program[0];
 }
 
-fn execute<'b>(program: &'b mut Vec<i32>, i: usize) {
-  let instruction = program[i];
-  //println!("Instruction {}", instruction);
+fn execute<'b>(program: &'b mut Vec<i32>, i: &mut usize) {
+  let instruction = program[*i];
   if instruction % 100 == 1 {
-      let oper1 = program[i+1] as usize;
-      let oper2 = program[i+2] as usize;
-      let retloc = program[i+3] as usize;
-      //println!("Adding, previous retloc {} value {}", retloc, program[retloc]);
+      let oper1 = program[*i+1] as usize;
+      let oper2 = program[*i+2] as usize;
+      let retloc = program[*i+3] as usize;
       program[retloc] = 
         match instruction % 1000 - 1 {
             100..=900 => oper1 as i32,
@@ -56,12 +60,11 @@ fn execute<'b>(program: &'b mut Vec<i32>, i: usize) {
             1000..=9000 => oper2 as i32,
             _           => program[oper2]
         };
-     // println!("Adding, new retloc {} value {}", retloc, program[retloc]);
   }
   if instruction % 100 == 2 {
-      let oper1 = program[i+1] as usize;
-      let oper2 = program[i+2] as usize;
-      let retloc = program[i+3] as usize;
+      let oper1 = program[*i+1] as usize;
+      let oper2 = program[*i+2] as usize;
+      let retloc = program[*i+3] as usize;
       program[retloc] = 
         match instruction % 1000 - 2 {
             100..=900 => oper1 as i32,
@@ -74,12 +77,73 @@ fn execute<'b>(program: &'b mut Vec<i32>, i: usize) {
         };
   }
   if instruction % 100 == 3 {
-      //println!("Storing input");
-      let retloc = program[i+1] as usize;
+      let retloc = program[*i+1] as usize;
       program[retloc] = INPUT;
   }
   if instruction % 100 == 4 {
-      let retloc = program[i+1] as usize;
+      let retloc = program[*i+1] as usize;
       println!("Output from inst 4 is {}", program[retloc]);
+  }
+  if instruction % 100 == 5 {
+      let oper1 = program[*i+1] as usize;
+      let retloc = program[*i+2] as usize;
+      let tester = match instruction % 1000 - 5 {
+          100..=900 => oper1 as i32,
+          _         => program[oper1]
+      };
+      if tester != 0 {
+          *i = match instruction % 10000 - 5 {
+              1000..=9000 => retloc - 3,
+              _ => program[retloc] as usize - 3
+          }
+      };
+  }
+  if instruction % 100 == 6 {
+      let oper1 = program[*i+1] as usize;
+      let retloc = program[*i+2] as usize;
+      let tester = match instruction % 1000 - 6 {
+          100..=900 => oper1 as i32,
+          _         => program[oper1]
+      };
+      if tester == 0 {
+          *i = match instruction % 10000 - 5 {
+              1000..=9000 => retloc - 3,
+              _ => program[retloc] as usize - 3
+          }
+      };
+  }
+  if instruction % 100 == 7 {
+      let oper1 = program[*i+1] as usize;
+      let oper2 = program[*i+2] as usize;
+      let retloc = program[*i+3] as usize;
+      let tester1 = match instruction % 1000 - 7 {
+          100..=900 => oper1 as i32,
+          _         => program[oper1]
+      };
+      let tester2 = match instruction % 10000 - 7 {
+          1000..=9000 => oper2 as i32,
+          _         => program[oper2]
+      };
+      program[retloc] = match tester1 < tester2 {
+          true => 1,
+          false => 0
+      };
+  }
+  if instruction % 100 == 8 {
+      let oper1 = program[*i+1] as usize;
+      let oper2 = program[*i+2] as usize;
+      let retloc = program[*i+3] as usize;
+      let tester1 = match instruction % 1000 - 8 {
+          100..=900 => oper1 as i32,
+          _         => program[oper1]
+      };
+      let tester2 = match instruction % 10000 - 8 {
+          1000..=9000 => oper2 as i32,
+          _         => program[oper2]
+      };
+      program[retloc] = match tester1 == tester2 {
+          true => 1,
+          false => 0
+      };
   }
 }
